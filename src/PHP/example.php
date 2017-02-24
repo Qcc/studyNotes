a1e7efd2dbe75386089362c9026b6d3ff58d965e
kevin <kevin.e@qq.com>
2017-02-24 13:36:28 +0800

    php

---
+++ src/PHP/example.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP</title>
+    </head>
+    <body>
+        <h4>my first page</h4>
+        <?php
+         
+         
+         echo "<ul><li>1111</li><li>12222</li><li>33333</li></ul>";
+
+        ?>
+    </body>
+</html>

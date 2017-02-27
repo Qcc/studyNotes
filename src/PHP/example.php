2b68ab842a2ff5e05f508405d154450aeb23fded
kevin <kevin.e@qq.com>
2017-02-27 15:28:24 +0800

    php

--- src/PHP/example.php
+++ src/PHP/example.php
@@ -1,16 +1,15 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML> 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>PHP</title>
-    </head>
-    <body>
-        <h4>my first page</h4>
-        <?php
-         
-         
-         echo "<ul><li>1111</li><li>12222</li><li>33333</li></ul>";
-
-        ?>
-    </body>
+<head>
+<meta charset="utf-8">
+<title>菜鸟教程(runoob.com)</title>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body> 
+    <?php
+    phpinfo();
+    ?>
+</body>
 </html>

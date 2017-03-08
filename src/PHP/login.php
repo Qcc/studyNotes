4f0581062777418f60f455f8344c37acd4cd1bbb
kevin <kevin.e@qq.com>
2017-03-08 16:32:45 +0800

    git

--- src/PHP/login.php
+++ src/PHP/login.php
@@ -9,36 +9,14 @@
 
 <body>
  <?php
-  $search_dir = '../';
-$contents = scandir($search_dir);
-print '<h2>目录</h2><ul>';
-foreach($contents as $item){
-    if((is_dir($search_dir . '/' . $item)) && (substr($item,0,1) != '.')){
-        print "<li>$item</li>\n";
-    }
-}
-print "</ul>";
-print '<hr /><h2>所有文件</h2>
-<table cellpadding="2" cellspacing = "2" align="left">
-<tr>
-<td>文件名</td>
-<td>大小</td>
-<td>修改时间</td>
-</tr>';
- 
-foreach($contents as $item){
-    if((is_file($search_dir . "/" . $item)) && (substr($item,0,1) !='.')){
-        $fs =filesize($search_dir . '/'. $item);
-        $lm = date('F j,Y',filemtime($search_dir . '/' . $item));
-        print "<tr>
-        <td>$item</td>
-        <td>$fs bytes</td>
-        <td>$lm</td>
-        </tr>\n";
-    }
-}
-print "</table><br />";
- 
+    $mysqli = new mysqli('localhost','root','','dbname');
+    $query='SELECT sku,name,price FROM products ORDER by name';
+    $result=$mysqli->query($query,MYSQLI_STORE_RESULT);
+
+    printf("一共查询到了%s行。<br />",$result->num_rows);
+    while(list($sku,$name,$price) = $result->fetch_row())
+        printf("(%s) %s:\$%s <br />",$sku,$name,$price);
+
   ?>
 
 </body>

6144e996a7dc6e0bfb5b272ca30443a2d026efb9
kevin-ubuntu <kevin.e@qq.com>
2017-04-12 17:51:00 +0800

    vue

--- src/NodeJS/myapp/app.js
+++ src/NodeJS/myapp/app.js
@@ -1,32 +1,8 @@
 var express = require('express');
 var app = express();
-
 app.set('views','./views');
 app.set('view engine','jade');
-
-
-app.use(function (req,res,next) {
-    console.log('i am Middleware');
-    next();
-});
-
-app.get('/',function (req,res) {
-    res.render('index',{title:'kevin',message:'沟通科技'});
-});
-var data ='all';
-app.all('/all',function (req,res,next) {
-    res.send('this is all');    
-});
-
-app.get('/main',function (req,res) {
-    res.send('this is main page!');
-});
-
-app.post('/post',function (req,res) {
-    res.send('this is post page!');
-});
-
-app.use('/file',express.static('public'));
+app.use(express.static('public'));
 
 var server = app.listen(3000,function () {
     var host = server.address().address;

be6378453e7d076381c3415ad07b92e733c78d37
kevin-ubuntu <kevin.e@qq.com>
2017-04-24 09:02:55 +0800

    mock

--- src/json-server/mock.js
+++ src/json-server/mock.js
@@ -6,6 +6,7 @@ module.exports = function () {
             licTemp:{"status":200,"errorCode":0,"message":"Success","moreInfo":"",allRecord:allRecord,entity:[]},
         }
     let products= ['云桌面','CTBS高级版','CTBS企业版'];
+    let active =['已激活','未激活'];
     for(let i=1;i<allRecord;i++){
         let content = Random.cparagraph(0,10);
         root.licTemp.entity.push({
@@ -16,7 +17,7 @@ module.exports = function () {
             product:products[Random.integer(0,2)], //产品
             trial: Random.datetime(), //授权结束日期
             license:Random.integer(1,120), //站点数
-            active:Random.boolean(), //是否已经激活
+            active:active[Random.integer(0,1)], //是否已经激活
         });
     }    
     return root;
